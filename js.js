9a39dcfd09f0e526ab2694b9d1120dc0f76a8773
Pigstain <edelwiss2@gmail.com>
2023-06-15 17:01:36 +0300

    grid looking like grid

--- js.js
+++ js.js
@@ -1,12 +1,18 @@
 const container = document.querySelector(".container");
 function grid(){
     for(let i=0;i<16;i++){
+        let rows=document.createElement("div");
         for(let j=0;j<16;j++){
             let divs=document.createElement("div");
             divs.textContent='0';
-            container.appendChild(divs);
+            divs.classList.add('square');
+            rows.appendChild(divs);
+            
         }
+        container.appendChild(rows);
     }
     
 }
 grid();
+const temp = document.querySelectorAll(".square");
+temp.textContent='9';
