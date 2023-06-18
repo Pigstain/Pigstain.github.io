1286acfb4974d52fcd3138ebf9e692ac3a98ae9f
Pigstain <edelwiss2@gmail.com>
2023-06-18 23:58:45 +0300

    resizing feature done

--- js.js
+++ js.js
@@ -5,41 +5,49 @@ function grid(size){
         for(let j=0;j<size;j++){
             let divs=document.createElement("div");
             divs.classList.add('square');
+            divs.addEventListener('mouseenter',()=>{
+                divs.classList.add('hover');
+            });
+            divs.addEventListener('click',()=>{
+                divs.classList.remove('hover');
+            });
             rows.appendChild(divs);
             
         }
         container.appendChild(rows);
     }
-    
-}
-grid(16);
-let temp = document.querySelectorAll('.square');
-
-temp.forEach(square => {
-    square.addEventListener('mouseenter',()=>{
-        square.classList.add('hover');
-    });
+    let New = 672/size -2;
+    New+='px';
+    const newTemp = document.querySelectorAll('.square');
+  newTemp.forEach(item =>{
+    item.style.width=New;
+    item.style.height=New;
 });
+}
 
-temp.forEach(item=>{
-   
-    item.addEventListener('click',()=>{
+ 
+
+
+
+function reset(){
+    const temp = document.querySelectorAll('.square');
+    temp.forEach(item => {
         item.classList.remove('hover');
     });
-});
-
+}
 
 function sizeChange(size){
-    for(let i=0;i<size;i++){
-        let rows=document.createElement("div");
-        for(let j=0;j<size;j++){
-            let divs=document.createElement("div");
-            divs.classList.add('square');
-            rows.appendChild(divs);
-            
-        }
-        container.appendChild(rows);
-    }
+    const temp = document.querySelectorAll('.square');
+    temp.forEach(item =>{
+        item.remove();
+    });
+  grid(size);
+
+  
 }
 const btn = document.querySelector('.sizebtn');
-btn.addEventListener('click',()=>sizeChange(32));
+btn.addEventListener('click',()=>sizeChange(12));
+
+const btn_reset = document.querySelector('.resetbtn');
+btn_reset.addEventListener('click',() =>reset());
+grid(16);
